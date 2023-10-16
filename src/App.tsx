cbce396599e527dc0a8941578ac7b9f19bc65b88
Bartosz <bartek2@onet.eu>
2023-10-16 19:42:55 +0200

    Add conditional to App.tsx file (very basic logic for displaying data)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "./components/Button";
 import Input from "./components/Input";
+import { noOp } from "./utils";
 
 function App() {
   const [str, setStr] = useState<string>("");
@@ -12,6 +13,25 @@ function App() {
       <Input value={str} onChange={(e) => setStr(e.target.value)} />
 
       <Button onClick={() => console.log("click")}>test</Button>
+
+      {false ? (
+        <div>Empty</div>
+      ) : (
+        <div>
+          <hr />
+          <div>
+            Name: <b></b>
+          </div>
+          <div>
+            ID:{" "}
+            <Input
+              value={"clnt5fbvl000008l3e4j41yow-270291013202628"}
+              readOnly
+              onChange={noOp}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
